3aaa069605f7e25cf31599792cfd7cf3db836402
Luiz <luizhsou1@gmail.com>
2020-06-22 22:31:26 -0300

    [parte 1] utilizando a biblioteca de fotos

--- Agenda/Alunos/Controller/AlunoViewController.swift
+++ Agenda/Alunos/Controller/AlunoViewController.swift
@@ -56,6 +56,19 @@ class AlunoViewController: UIViewController, ImagePickerFotoSelecionada {
         self.scrollViewPrincipal.contentSize = CGSize(width: self.scrollViewPrincipal.frame.width, height: self.scrollViewPrincipal.frame.height + self.scrollViewPrincipal.frame.height/2)
     }
     
+    func mostrarMultimidia(_ opcao: MenuDeOpcoes) {
+        let multimidia = UIImagePickerController()
+        multimidia.delegate = imagePicker
+        
+        if opcao == .camera && UIImagePickerController.isSourceTypeAvailable(.camera) {
+            multimidia.sourceType = .camera
+            
+        } else {
+            multimidia.sourceType = .photoLibrary
+        }
+        self.present(multimidia, animated: true, completion: nil)
+    }
+    
     // MARK: - Delegate
     
     func imagePickerFotoSelecionada(_ foto: UIImage) {
@@ -65,12 +78,10 @@ class AlunoViewController: UIViewController, ImagePickerFotoSelecionada {
     // MARK: - IBActions
     
     @IBAction func buttonFoto(_ sender: UIButton) {
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            let multimidia = UIImagePickerController()
-            multimidia.sourceType = .camera
-            multimidia.delegate = imagePicker
-            self.present(multimidia, animated: true, completion: nil)
-        }        
+        let menu = ImagePicker().menuDeOpcoes { (opcao) in
+            self.mostrarMultimidia(opcao)
+        }
+        present(menu, animated: true, completion: nil)
     }
     
     @IBAction func stepperNota(_ sender: UIStepper) {
--- Agenda/Components/ImagePicker.swift
+++ Agenda/Components/ImagePicker.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+enum MenuDeOpcoes {
+    case camera
+    case biblioteca
+}
+
 protocol ImagePickerFotoSelecionada {
     func imagePickerFotoSelecionada(_ foto: UIImage)
 }
@@ -17,13 +22,29 @@ class ImagePicker: NSObject, UIImagePickerControllerDelegate, UINavigationContro
     // MARK: - Atributos
     var delegate: ImagePickerFotoSelecionada?
     
-    // MARK: - Construtor
-    
     // MARK: - Métodos
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         let foto = info[UIImagePickerControllerOriginalImage] as! UIImage
         
         delegate?.imagePickerFotoSelecionada(foto)
         picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func menuDeOpcoes(completion: @escaping(_ opcao: MenuDeOpcoes) -> Void) -> UIAlertController {
+        let menu = UIAlertController(title: "Atenção", message: "escolha uma das opções abaixo", preferredStyle: .actionSheet)
+        let camera = UIAlertAction(title: "tirar foto", style: .default, handler: {
+            (acao) in
+            completion(.camera)
+        })
+        menu.addAction(camera)
+        let biblioteca = UIAlertAction(title: "biblioteca", style: .default, handler: {
+            (acao) in
+            completion(.biblioteca)
+        })
+        menu.addAction(biblioteca)
+        let cancelar = UIAlertAction(title: "cancelar", style: .cancel, handler: nil)
+        menu.addAction(cancelar)
+        
+        return menu
     }
 }
