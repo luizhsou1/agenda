109ee03d277e2312e10f4edc9d86dc9dc9914709
Luiz <luizhsou1@gmail.com>
2020-06-23 15:11:21 -0300

    [parte 4] configurando mapa

--- Agenda/Mapa/Controller/MapaViewController.swift
+++ Agenda/Mapa/Controller/MapaViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import MapKit
 
 class MapaViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var mapa: MKMapView!
     
     // MARK: - Variáveis
     var aluno: Aluno?
@@ -18,11 +23,38 @@ class MapaViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = getTitulo()
-        print(aluno?.nome)
+        self.localizacaoInicial()
+        self.localizarAluno()
     }
     
+    // MARK: - Métodos
     func getTitulo() -> String {
         return "Localizar Alunos"
     }
-
+    
+    func localizacaoInicial() {
+        Localizacao().converteEnderecoEmCoordenadas(endereco: "Parque do Sabiá - Uberlândia") { (localizacaoEncontrada) in
+            let pino = self.configuraPino(titulo: "Parque do Sabiá", localizacao: localizacaoEncontrada)
+            let regiao = MKCoordinateRegionMakeWithDistance(pino.coordinate, 10000, 10000)
+            self.mapa.setRegion(regiao, animated: true)
+            self.mapa.addAnnotation(pino)
+        }
+    }
+    
+    func localizarAluno() {
+        if let aluno = aluno {
+            Localizacao().converteEnderecoEmCoordenadas(endereco: aluno.endereco!) { (localizacaoEncontrada) in
+                let pino = self.configuraPino(titulo: aluno.nome!, localizacao: localizacaoEncontrada)
+                self.mapa.addAnnotation(pino)
+            }
+        }
+    }
+    
+    func configuraPino(titulo: String, localizacao: CLPlacemark) -> MKPointAnnotation {
+        let pino = MKPointAnnotation()
+        pino.title = titulo
+        pino.coordinate = localizacao.location!.coordinate
+        
+        return pino
+    }
 }
