2d7e5add2c5cfe6168366b00a1f9098a7edc1c19
Luiz <luizhsou1@gmail.com>
2020-06-23 11:02:56 -0300

    [parte 2] comecando com o Cora Data

--- Agenda/Alunos/Controller/AlunoViewController.swift
+++ Agenda/Alunos/Controller/AlunoViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class AlunoViewController: UIViewController, ImagePickerFotoSelecionada {
     
@@ -25,6 +26,10 @@ class AlunoViewController: UIViewController, ImagePickerFotoSelecionada {
     
     // MARK: - Atributos
     
+    var contexto: NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
     let imagePicker = ImagePicker()
     
     // MARK: - View Lifecycle
@@ -89,4 +94,21 @@ class AlunoViewController: UIViewController, ImagePickerFotoSelecionada {
     }
     
     
+    @IBAction func buttonSalvar(_ sender: UIButton) {
+        let aluno = Aluno(context: contexto)
+        aluno.nome = textFieldNome.text
+        aluno.endereco = textFieldEndereco.text
+        aluno.telefone = textFieldTelefone.text
+        aluno.site = textFieldSite.text
+        aluno.nota = (textFieldNota.text! as NSString).doubleValue
+        aluno.foto = imageAluno.image
+        
+        do {
+            try contexto.save()
+            navigationController?.popViewController(animated: true)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+    }
 }
